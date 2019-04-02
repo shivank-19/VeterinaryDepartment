4e48165889e86a1fc2eeaeee98fa9be1a7036aae
RenuTiwari1401 <renu.tiwari_mca18@gla.ac.in>
2019-04-03 00:46:42 +0530

    second commit

--- app/src/main/java/com/e/veterinarydepartment/MainActivity.java
+++ app/src/main/java/com/e/veterinarydepartment/MainActivity.java
@@ -1,24 +1,35 @@
 package com.e.veterinarydepartment;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        TextInputEditText username , password;
+        final TextInputLayout username , password;
         Button loginbutton;
+        final FirebaseAuth mAuth;
 
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
@@ -27,9 +38,34 @@ public class MainActivity extends AppCompatActivity {
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, secondActivity.class);
-                startActivity(intent);
-                finish();
+
+                mAuth.signInWithEmailAndPassword(username.getEditText().getText().toString(), password.getEditText().getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                   // Log.d(TAG, "signInWithEmail:success");
+                                   // FirebaseUser user = mAuth.getCurrentUser();
+                                 //   updateUI(user);
+
+                                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                  //  Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                     Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                 //   updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
+
+
             }
         });
     }
--- app/src/main/java/com/e/veterinarydepartment/SecondActivity.java
+++ app/src/main/java/com/e/veterinarydepartment/SecondActivity.java
@@ -2,12 +2,19 @@ package com.e.veterinarydepartment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class secondActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
     }
 }
